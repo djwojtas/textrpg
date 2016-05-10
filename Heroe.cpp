1341355685d47deda320e3e18d18f1be7dccf489
szymonsadowski3 <szymonsadowski3@gmail.com>
2016-05-10 08:39:31 +0200

    Add files via upload
    Updated artefact boosting

--- Heroe.cpp
+++ Heroe.cpp
@@ -7,9 +7,15 @@ using namespace std;
 Heroe::Heroe(string n, int s, int a, int hpoints)
 {
     name=n;
+
     strength=s;
     agility=a;
     hp=hpoints;
+
+    strength_boost=0;
+    agility_boost=0;
+    hp_boost=0;
+
     rune_count=0;
 }
 
@@ -26,6 +32,21 @@ void Heroe::setAgility(int a)
 void Heroe::setHP(int hpoints)
 {
     hp=hpoints;
+}
+
+void Heroe::setStrengthBoost(int sb)
+{
+    strength_boost=sb;
+}
+
+void Heroe::setAgilityBoost(int ab)
+{
+    agility_boost=ab;
+}
+
+void Heroe::setHPBoost(int hpb)
+{
+    hp_boost=hpb;
 }
 
 void Heroe::setRuneCount(int r)
@@ -48,6 +69,21 @@ int Heroe::getHP(int hpoints)
     return hp;
 }
 
+int Heroe::getStrengthBoost()
+{
+    return strength_boost;
+}
+
+int Heroe::getAgilityBoost()
+{
+    return agility_boost;
+}
+
+int Heroe::getHPBoost()
+{
+    return hp_boost;
+}
+
 int Heroe::getRuneCount(int r)
 {
     return rune_count;
@@ -56,15 +92,28 @@ int Heroe::getRuneCount(int r)
 void Heroe::addArtefact(Artefact &ar)
 {
     artefact_list.push_back(ar);
+
+    if(ar.getSkillBoost()==STRENGTH)
+    {
+        this->strength_boost=ar.getBoostAmount();
+    } else
+    if (ar.getSkillBoost()==AGILITY)
+    {
+        this->agility_boost=ar.getBoostAmount();
+    } else
+    if (ar.getSkillBoost()==HP)
+    {
+        this->hp_boost=ar.getBoostAmount();
+    }
 }
 
 void Heroe::printHeroe()
 {
     cout<<endl;
     cout<<"Name = "<<name<<endl;
-    cout<<"Strength = "<<strength<<endl;
-    cout<<"Agility = "<<agility<<endl;
-    cout<<"Health Points = "<<hp<<endl;
+    cout<<"Strength = "<<strength<<" (+"<<strength_boost<<") "<<endl;
+    cout<<"Agility = "<<agility<<" (+"<<agility_boost<<") "<<endl;
+    cout<<"Health Points = "<<" (+"<<hp_boost<<") "<<endl;
     cout<<"Possessed Runes = "<<rune_count<<endl;
 
     cout<<endl;
--- Heroe.h
+++ Heroe.h
@@ -9,9 +9,16 @@ using namespace std;
 class Heroe
 {
 private:
+    //Own Skills
     int strength;
     int agility;
     int hp;
+
+    //Artefact boosts
+    int strength_boost;
+    int agility_boost;
+    int hp_boost;
+
     vector <Artefact> artefact_list;
     int rune_count;
     string name;
@@ -22,13 +29,25 @@ public:
     void setStrength(int s);
     void setAgility(int a);
     void setHP(int hpoints);
+
+    void setStrengthBoost(int sb);
+    void setAgilityBoost(int ab);
+    void setHPBoost(int hpb);
+
     void setRuneCount(int r);
+    //END OF SETTERS-------------
 
     //Getters
     int getStrength(int s);
     int getAgility(int a);
     int getHP(int hpoints);
+
+    int getStrengthBoost();
+    int getAgilityBoost();
+    int getHPBoost();
+
     int getRuneCount(int r);
+    //END OF GETTERS-----------
 
     //Utility
     void addArtefact(Artefact &ar);
