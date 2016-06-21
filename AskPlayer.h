e5560836f1855d84a5affcc81f3113a56b658880
djwojtas <djwojtas-96@o2.pl>
2016-06-21 21:14:19 +0200

    dodanie doxy do WriteOut i AskPlayer

--- AskPlayer.h
+++ AskPlayer.h
@@ -1,24 +1,77 @@
+/**
+* \file AskPlayer.h
+* \author RPGteam
+* \date 21.06.2016
+* \version 1.0
+* \brief File contains the definition of AskPlayer class
+*/
+// -------------------------------------------------------------------------
+
 #ifndef ASKPLAYER_H
 #define ASKPLAYER_H
 
 using namespace std;
 
+/**
+* \class AskPlayer
+* \author RPGteam
+* \date 21.06.2016
+* \brief AskPlayer class gives API for communication with player
+*
+*   AskPlayer handles mostly two things. It takes care of formating output, like when to add endline or insert some signs.
+*   Also, it can be used for getting information from player and assuring that data is (probably) valid.
+*   Class should be used by calling "ask" global object. Eg. ask.say("Hello");
+*/
 class AskPlayer
 {
-  int state = 0;
+  int state = 0; ///< State is 1 when last message was from computer and 0 when from user
 public:
+  /// \brief State setter, can be used to override default class settings
+  /** State setter can be used to override default class settings */
+  /// \param state_ - 0 or anything - it's used as boolean in case there are need to use more states
   void setState(int state_);
 
+  /// \brief Print's message as Player
+  /** Method will try to separate sentence from NPC and computer sentences. It should look like player is filling cin*/
+  /// \param message - message that player should say
   void playerSay(string message);
 
+  /// \brief Print's message as NPC
+  /** Method will try to separate sentence from player's.*/
+  /// \param message - message that computer should say
   void say(string message);
 
+  /// \brief Print's message as Narrator
+  /** Method will try to separate sentence from player's.*/
+  /// \param message - message that narrator should say
   void narrate(string message);
 
+  /// \brief Asks player for INT from certain range and validates it
+  /** Method will write message as NPC than player will have to write answer. If answer was correct (from specified range
+  * and same INT type) function will return. If not - fail_message will be written and process starts again */
+  /// \param message - question that computer should ask
+  /// \param fail_message - sentence that computer should say after invalid answer
+  /// \param minimum - minimum of range that INT should be from (more or equal)
+  /// \param message - maximum of range that INT should be from (less or equal)
+  /// \return Returns int that player provided
   int askForInt(string message, string fail_message, int minimum, int maximum);
 
+  /// \brief Asks player for double from certain range and validates it
+  /** Method will write message as NPC than player will have to write answer. If answer was correct (from specified range
+  * and same double type) function will return. If not - fail_message will be written and process starts again */
+  /// \param message - question that computer should ask
+  /// \param fail_message - sentence that computer should say after invalid answer
+  /// \param minimum - minimum of range that double should be from (more or equal)
+  /// \param message - maximum of range that double should be from (less or equal)
+  /// \return Returns double that player provided
   double askForDouble(string message, string fail_message, double minimum, double maximum);
 
+  /// \brief Asks player for string.
+  /** Method will write message as NPC than player will have to write answer. If answer was correct (if cin won't produce
+  * error) function will return. If not - fail_message will be written and process starts again */
+  /// \param message - question that computer should ask
+  /// \param fail_message - sentence that computer should say after invalid answer
+  /// \return Returns string that player provided
   string askForString(string message, string fail_message);
 };
 
--- WriteOut.h
+++ WriteOut.h
@@ -1,3 +1,12 @@
+/**
+* \file WriteOut.h
+* \author RPGteam
+* \date 21.06.2016
+* \version 1.0
+* \brief File contains the definition of WriteOut class
+*/
+// -------------------------------------------------------------------------
+
 #ifndef WRITEOUT_H
 #define WRITEOUT_H
 
@@ -5,15 +14,33 @@
 
 using namespace std;
 
+/**
+* \class WriteOut
+* \author RPGteam
+* \date 21.06.2016
+* \brief WriteOut class provide ability of sequential text writing (letter after letter)
+*
+*   WriteOut can be used like mostly like cout (but accepts only strings, use to_string(any type) to convert)
+*   to write text letter after letter with specified time delay (default is 30)
+*   Class should be used by calling "write" global object. Eg. write<<"Hello"; or write.setSleepTime(60);
+*/
 class WriteOut
 {
-  int sleep_time = 30;
+  int sleep_time = 30; ///< How much delay in miliseconds should program wait between printing letters
 
 public:
+  /// \brief Sleep time setter
+  /// \param sleep_time_ - time to wait between letters in miliseconds
   void setSleepTime(int sleep_time_);
 
+  /// \brief Function cout's text letter by letter with delay of sleep_time
+  /// \param text - string to print letter by letter
   void write(string text);
 
+  /// \brief Overload of << operator to make write look like cout
+  /// \param output - object (probably global - write) that will write out input
+  /// \param input - sentence to write out
+  /// \return returns reference to object that writes out, so function can be used like write<<"smth"<<smth else";
   friend WriteOut &operator<<(WriteOut &output, const string input);
 };
 
