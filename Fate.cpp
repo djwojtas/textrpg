f0a3f3fd160ac7f64c79343229109fbf21bfd732
szymonsadowski3 <szymonsadowski3@gmail.com>
2016-06-22 20:53:29 +0200

    Added Guessing Game to Fate

--- Fate.cpp
+++ Fate.cpp
@@ -23,32 +23,64 @@ void Fate::play(Heroe& subject)
     }
 
     srand( time( NULL ) );
-    setGoodOrBadLuck(rand()%2);
+    int computer_number = rand()%101;
+    int success=0;
+    int counter=0;
+
+    //-----GUESSING GAME------//
+    ask.narrate("Odd stranger tackles you on the road.");
+    ask.say("Hello wanderer, let's play the Guessing Game!\nYou have 7 tries.");
+
+    do
+    {
+        int user_number = ask.askForInt("Tell me number from 0 to 100.", "Do you can even count to 100?", 0, 100);
+        if(user_number>computer_number)
+        {
+            ask.say("That is too big");
+        }
+        else if(user_number<computer_number)
+        {
+            ask.say("That is too small");
+        }
+        else
+        {
+            success = 1;
+            break;
+        }
+        counter++;
+    } while (counter!=6);
+    //-----END OF GUESSING GAME------//
+
+    setGoodOrBadLuck(success);
 
     if(good_or_bad_luck==1) //Szczescie
     {
-        ask.say("Good fortune!");
+        ask.say("You have won traveler! You're good!");
         int drawn_number=rand()%5; //0 to 4;
 
-        if(drawn_number==0) /*20% na zdobycie artefaktu*/
+        if(drawn_number!=0) /*80% na zdobycie artefaktu*/
         {
             int index_of_artefact=( rand() % BaseOfArtefacts.getSize() );
             Artefact drawn_artefact=BaseOfArtefacts.getArtefact(index_of_artefact);
             subject.addArtefact(drawn_artefact);
-            ask.say("you've received: ");
+            ask.say("I will give you this: ");
             drawn_artefact.printArtefact();
             write<<"\n\n";
             BaseOfArtefacts.popArtefact(index_of_artefact);
+        }
+        else
+        {
+            ask.say("Unfortunately, right now i do not have any Artefact for you...\n\n");
         }
     }
     else //Pech
     {
         /*Utrata artefaktow lub zachowanie artefaktow*/
-        ask.say("Bad fortune!");
+        ask.say("Bad guessing! You have lost!");
 
-        int drawn_number=rand()%5; //0 to 4;
+        int drawn_number=rand()%3; //0 or 1;
 
-        if(drawn_number==0) /*20% na utratę artefaktu*/
+        if(drawn_number==0) /*1/3 szansy na utratę artefaktu*/
         {
             if(subject.getAmountOfArtefacts()==0)
             {
@@ -60,14 +92,14 @@ void Fate::play(Heroe& subject)
             {
                 int artefact_index = rand() % subject.getAmountOfArtefacts();
                 BaseOfArtefacts.pushArtefact( subject.loseArtefact(artefact_index) );
-                ask.say("Unfortunately, you've lost this artefact!: ");
+                ask.say("I regret to say it, but i must take you this artefact!: ");
                 BaseOfArtefacts.getArtefact(BaseOfArtefacts.getSize()-1).printArtefact();
                 write<<"\n\n";
             }
         }
         else
         {
-            ask.say("Fortunately, you've saved your artefacts!");
+            ask.say("Umm, i do not feel quite mighty today. I will not take you any Artefact. Now go away!\n\n");
         }
     }
 }
